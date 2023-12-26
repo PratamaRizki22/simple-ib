283ba8a8805281144e4a44a4885e0e0e593d88e6
PratamaRizki22 <rizkipurnomo914@gmail.com>
2023-12-26 10:33:09 +0700

    inisisasin

---
+++ src/SimpleLibrary.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SimpleLibrary;
+
+class SimpleLibrary
+{
+    public function sayHello()
+    {
+        return "Hello from SimpleLibrary!";
+    }
+
+    public function addNumbers($a, $b)
+    {
+        return $a + $b;
+    }
+}
